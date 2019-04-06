7a017e6793d07f4a6523fdf3e7d8fffc5f5bdb17
Dustin Walker <dustinwalkerart@gmail.com>
2019-04-06 12:42:08 +0900

    moves files

--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+//Containers
+import CenteredColumnContainer from './containers/CenteredColumnContainer';
+import ColumnContainer from './containers/ColumnContainer';
+import RowContainer from './containers/RowContainer';
+//Sections
+import ColumnSection from './sections/ColumnSection';
+import ColumnSectionWithHeader from './sections/ColumnSectionWithHeader';
+import RowSection from './sections/RowSection';
+import RowSectionWithHeader from './sections/RowSectionWithHeader';
+//Cards
+import Card from './cards/Card';
+import CardWithHeader from './cards/CardWithHeader';
+//Inputs
+import Input from './inputs/Input';
+//Buttons
+import Button from './buttons/Button';
+//Media
+import Image from './media/Image';
+import ImageCircle from './media/ImageCircle';
+//Modals
+import Modal from './modals/Modal';
+//Tags
+import Tag from './tags/Tag';
+//Dropdowns
+import DropDown from './dropdowns/DropDownItem'
 
-import styles from './styles.css'
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+export {
+  //Containers
+  CenteredColumnContainer,
+  ColumnContainer,
+  RowContainer,
+  //Sections
+  ColumnSection,
+  ColumnSectionWithHeader,
+  RowSection,
+  RowSectionWithHeader,
+  //Cards
+  Card,
+  CardWithHeader,
+  //Inputs
+  Input,
+  //Buttons
+  Button,
+  //Media
+  Image,
+  ImageCircle,
+  //Modals
+  Modal,
+  //Tags
+  Tag,
+  //Dropdowns
+  DropDown,
 }
