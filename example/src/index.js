3e120b63305e37ccd5cca6572135f7af2449a804
Dustin Walker <dustinwalkerart@gmail.com>
2019-04-06 12:47:58 +0900

    fixes example

--- example/src/index.js
+++ example/src/index.js
@@ -1,7 +1,54 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+//Containers
+import CenteredColumnContainer from './containers/CenteredColumnContainer';
+import ColumnContainer from './containers/ColumnContainer';
+import RowContainer from './containers/RowContainer';
+//Sections
+import ColumnSection from './sections/ColumnSection';
+import ColumnSectionWithHeader from './sections/ColumnSectionWithHeader';
+import RowSection from './sections/RowSection';
+import RowSectionWithHeader from './sections/RowSectionWithHeader';
+//Cards
+import Card from './cards/Card';
+import CardWithHeader from './cards/CardWithHeader';
+//Inputs
+import Input from './inputs/Input';
+//Buttons
+import Button from './buttons/Button';
+//Media
+import Image from './media/Image';
+import ImageCircle from './media/ImageCircle';
+//Modals
+import Modal from './modals/Modal';
+//Tags
+import Tag from './tags/Tag';
+//Dropdowns
+import DropDown from './dropdowns/DropDownItem'
 
-import './index.css'
-import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+export {
+  //Containers
+  CenteredColumnContainer,
+  ColumnContainer,
+  RowContainer,
+  //Sections
+  ColumnSection,
+  ColumnSectionWithHeader,
+  RowSection,
+  RowSectionWithHeader,
+  //Cards
+  Card,
+  CardWithHeader,
+  //Inputs
+  Input,
+  //Buttons
+  Button,
+  //Media
+  Image,
+  ImageCircle,
+  //Modals
+  Modal,
+  //Tags
+  Tag,
+  //Dropdowns
+  DropDown,
+}
