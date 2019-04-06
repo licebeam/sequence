78ca9f39e013c43bf307edad3f9047269a73b891
Dustin Walker <dustinwalkerart@gmail.com>
2019-04-06 12:10:57 +0900

    adds existing components

---
+++ src/Components/index.js
@@ -0,0 +1,54 @@
+//Containers
+import CenteredColumnContainer from './containers/CenteredColumnContainer';
+import ColumnContainer from './containers/ColumnContainer';
+import RowContainer from './containers/RowContainer';
+//Sections
+import ColumnSection from './sections/ColumnSection';
+import ColumnSectionWithHeader from './sections/ColumnSectionWithHeader';
+import RowSection from './sections/RowSection';
+import RowSectionWithHeader from './sections/RowSectionWithHeader';
+//Cards
+import Card from './cards/Card';
+import CardWithHeader from './cards/CardWithHeader';
+//Inputs
+import Input from './inputs/Input';
+//Buttons
+import Button from './buttons/Button';
+//Media
+import Image from './media/Image';
+import ImageCircle from './media/ImageCircle';
+//Modals
+import Modal from './modals/Modal';
+//Tags
+import Tag from './tags/Tag';
+//Dropdowns
+import DropDown from './dropdowns/DropDownItem'
+
+
+export {
+  //Containers
+  CenteredColumnContainer,
+  ColumnContainer,
+  RowContainer,
+  //Sections
+  ColumnSection,
+  ColumnSectionWithHeader,
+  RowSection,
+  RowSectionWithHeader,
+  //Cards
+  Card,
+  CardWithHeader,
+  //Inputs
+  Input,
+  //Buttons
+  Button,
+  //Media
+  Image,
+  ImageCircle,
+  //Modals
+  Modal,
+  //Tags
+  Tag,
+  //Dropdowns
+  DropDown,
+}
---
+++ src/Components/modals/Modal.jsx
@@ -0,0 +1,46 @@
+import React, { Component, Fragment } from 'react';
+import styled from 'styled-components';
+import { colors } from '../constants';
+import '../base.css';
+
+const ModalFadeBackground = styled.div`
+ position: fixed; 
+ background-color: black;
+ opacity: .5;
+ top: 0;
+ left: 0;
+ width: 100vw;
+ height: 100vh;
+ overflow: hidden;
+ z-index: 10;
+ pointer-events: auto;
+`
+const FlexContainer = styled.div`
+  z-index:20;
+  display: flex;
+  flex-direction: row;
+  overflow: auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  padding: .2rem;
+  transform: translate(-50%, -50%);
+  border-radius: .2rem;
+  background-color: ${props => props.backgroundColor || colors.defaultContainerColor};
+`
+
+class Modal extends Component {
+  render() {
+    const { backgroundColor } = this.props;
+    return (
+      <Fragment>
+        <ModalFadeBackground />
+        <FlexContainer backgroundColor={backgroundColor}>
+          {this.props.children}
+        </FlexContainer>
+      </Fragment>
+    )
+  }
+}
+
+export default Modal
