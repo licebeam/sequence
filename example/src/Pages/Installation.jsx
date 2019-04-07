d5b00d392c080ac21e53cdc825ee76a651118a4d
Dustin Walker <dustinwalkerart@gmail.com>
2019-04-07 22:42:11 +0900

    Adjusts about and installation page

--- example/src/Pages/Installation.jsx
+++ example/src/Pages/Installation.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
 import {
   DropDown,
   Tag,
@@ -21,7 +22,24 @@ import {
 
 class Installation extends Component {
   render() {
-    return <CenteredColumnContainer>INSTALLATION</CenteredColumnContainer>;
+    const exampleCode = "yarn add sequence-ui";
+    const exampleCode2 = "npm install --save sequence-ui";
+    return (
+      <ColumnContainer>
+        <ColumnSectionWithHeader title="Installation" sectionMarginTop={40}>
+          <Text textSize="1" isBold>
+            Installing sequence-ui is very easy! In your project run either of
+            the following commands in the terminal.
+          </Text>
+          <SyntaxHighlighter wrapLines={true} language="bash">
+            {exampleCode}
+          </SyntaxHighlighter>
+          <SyntaxHighlighter wrapLines={true} language="bash">
+            {exampleCode2}
+          </SyntaxHighlighter>
+        </ColumnSectionWithHeader>
+      </ColumnContainer>
+    );
   }
 }
 
