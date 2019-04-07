82608ba13832d7799f50dfe818c61c6a8997ddf3
Dustin Walker <dustinwalkerart@gmail.com>
2019-04-07 15:41:00 +0900

    removes background from image.jsx

--- src/Image.jsx
+++ src/Image.jsx
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { colors } from './constants';
-import './base.css';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { colors } from "./constants";
+import "./base.css";
 
 const FlexContainer = styled.div`
   display: flex;
   flex-direction: row;
   overflow: auto;
-  margin: .2rem;
+  margin: 0.2rem;
   align-self: center;
   flex: 1;
-  width: ${props => props.imageWidth ? props.imageWidth + 'px' : '100px'};
-  height: ${props => props.imageHeight ? props.imageHeight + 'px' : '100px'};
-  background-color: ${props => props.backgroundColor || colors.defaultContainerColor};
-  .image{
-    width: ${props => props.imageWidth ? props.imageWidth + 'px' : '100px'};
-    height: ${props => props.imageHeight ? props.imageHeight + 'px' : '100px'};
+  width: ${props => (props.imageWidth ? props.imageWidth + "px" : "100px")};
+  height: ${props => (props.imageHeight ? props.imageHeight + "px" : "100px")};
+  .image {
+    width: ${props => (props.imageWidth ? props.imageWidth + "px" : "100px")};
+    height: ${props =>
+      props.imageHeight ? props.imageHeight + "px" : "100px"};
     object-fit: cover;
   }
-`
+`;
 
 class Image extends Component {
   render() {
-    const { backgroundColor, imageURL, imageWidth, imageHeight } = this.props;
+    const { imageURL, imageWidth, imageHeight } = this.props;
     return (
-      <FlexContainer backgroundColor={backgroundColor} imageWidth={imageWidth} imageHeight={imageHeight}>
+      <FlexContainer imageWidth={imageWidth} imageHeight={imageHeight}>
         <img className="image" src={imageURL} alt={imageURL} />
       </FlexContainer>
-    )
+    );
   }
 }
 
-export default Image
+export default Image;
